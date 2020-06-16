794f631a1db3e6985e91c9dd5b9b7b63b0e79dcd
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-06-16 19:32:38 -0400

    chapter 2 studio parts 1 and 2 complete

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+    private static final Double PI = 3.14;
+
+    public static Double getArea(Double radius) {
+        return PI * radius * radius;
+    }
+}
